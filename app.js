b46241a0174d29e90ae7dd149db886add8e995ee
alejandrolederman <alejandro.lederman@live.com.ar>
2021-10-19 15:24:52 -0300

    trabajo 6

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const path = require('path');
 
 
-app.listen(3001, ()=>{
+app.listen(3002, ()=>{
     console.log('Servidor funcionando');
 });
 
-// app.get('/', (req,res)=>{
-//     res.sendFile(__dirname + '/views/home.html');
-// });
+ app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+ });
 
-// app.get('/', (req,res)=>{
-//     res.sendFile(__dirname + '/views/register.html');
-// });
-
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+  app.get('/views/register.html', (req,res)=>{
+     res.sendFile(path.join(__dirname, '/views/register.html'))
+    
 });
+  
+app.get('/views/login.html', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+ });
