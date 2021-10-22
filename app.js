591a21776db2e588e75bc593cd0b4370122e51ff
alejandrolederman <alejandro.lederman@live.com.ar>
2021-10-21 21:33:29 -0300

    port

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ const app = express();
 app.use(express.static('public'));
 const path = require('path');
 
+const port = process.env.PORT || 3000;
 
-app.listen(3002, ()=>{
+
+ app.listen(port, ()=>{
     console.log('Servidor funcionando');
-});
+ });
 
  app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
  });
 
-  app.get('/views/register.html', (req,res)=>{
-     res.sendFile(path.join(__dirname, '/views/register.html'))
+//   app.get('/views/register.html', (req,res)=>{
+//      res.sendFile(path.join(__dirname, '/views/register.html'))
     
-});
+// });
   
-app.get('/views/login.html', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/login.html'))
- });
+// app.get('/views/login.html', (req,res)=>{
+//     res.sendFile(path.join(__dirname, '/views/login.html'))
+//  });
+
